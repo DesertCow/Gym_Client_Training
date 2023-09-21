d0aa40c91cfed9bdc7c084db491256668ef318ce
Clayton S <desertcow07@gmail.com>
2023-09-21 08:42:30 -0700

    Added Pages and login place holder page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
+import React, { useState } from "react";
+// import { LOGIN_M } from '../utils/mutations';
+// import { useMutation } from '@apollo/client';
+
+// import LoginPage from "../components/LoginPage";
+
+
+const Login = (props) => {
+
+
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center">
+
+      {/* <LoginPage /> */}
+
+      <h1 className="text-center mt-5"> Login Page</h1>
+
+    </div>
+  )
+};
+
+
+export default Login;
+
+//!========================= EOF =========================
