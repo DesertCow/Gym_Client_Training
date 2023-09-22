68aac5d75cc8372f7b5f95337f82ef99aa9977b7
Clayton S <desertcow07@gmail.com>
2023-09-22 13:28:59 -0700

    EOS

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,7 +19,36 @@ function Home() {
 
         <SideBar/>
 
-        <h1 className="col text-center mt-5 welcomeText"> Welcome Clayton</h1>
+        <div className="w-100">
+
+          <h1 className="text-center mt-5 welcomeText"> Welcome Clayton</h1>
+
+          <div className="d-flex justify-content-around align-items-center">
+
+            <div className="flex-column">
+
+              <div className="workoutBox mt-5">
+
+                <p className="text-center todaysBoxTitle"> Workout</p>
+
+              </div>
+
+            </div>
+
+            <div className="flex-column">
+
+              <div className="workoutBox mt-5">
+
+                <p className="text-center todaysBoxTitle"> Diet</p>
+
+              </div>
+
+            </div>
+
+          </div>
+
+
+        </div>
 
       </div>
       
