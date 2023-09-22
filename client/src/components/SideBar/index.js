f7ae17d00c23e95370e4c2cc152b2ad9218fafca
Clayton S <desertcow07@gmail.com>
2023-09-21 21:08:43 -0700

    Added Logout and User Settings icons / setup respective routing

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -8,6 +8,14 @@ const SideBar = () => {
 
   const navigate = useNavigate();
 
+  const logOutConfirm = async (event) => {
+
+    event.preventDefault();
+
+    console.log('Log Out Request!')
+
+  }
+
   return (
 
     // <h1 className="text-center mt-5">Side Bar</h1>
@@ -23,6 +31,21 @@ const SideBar = () => {
         <p className="sideBarLink mt-4"  onClick={() => navigate("/trainerSchedule")}>Trainer Schedule</p>
 
         <p className="sideBarLink mt-4"  onClick={() => navigate("/billing")}>Billing</p>
+
+        <div className="row mt-5 justify-content-around">
+
+
+          <img src={require("../../img/icons/logout_icon.png")}
+            className=" sidebar_footer_logo"
+            onClick={() => logOutConfirm(event)}
+            alt="Settings Icon" />
+          <img src={require("../../img/icons/settings_icon.png")}
+            className="sidebar_footer_logo"
+            onClick={() => navigate("/userSettings")}
+            height="50px"
+            alt="Settings Icon" />
+
+        </div>
 
       </div>
 
