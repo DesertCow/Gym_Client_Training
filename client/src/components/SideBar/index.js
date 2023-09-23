0e1877e60506b66f3ec90e15f3c0ce31be4c8b75
Clayton S <desertcow07@gmail.com>
2023-09-22 20:55:05 -0700

    Added Hover effect to logout and user settings links in sidebar

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -36,11 +36,11 @@ const SideBar = () => {
 
 
           <img src={require("../../img/icons/logout_icon.png")}
-            className=" sidebar_footer_logo_logout"
+            className=" sidebar_footer_logo_logout sidebar_footer_logo"
             onClick={() => logOutConfirm(event)}
             alt="Settings Icon" />
           <img src={require("../../img/icons/settings_icon.png")}
-            className="sidebar_footer_logo_settings"
+            className="sidebar_footer_logo_settings sidebar_footer_logo"
             onClick={() => navigate("/userSettings")}
             alt="Settings Icon" />
 
